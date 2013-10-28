7643a337a1aea199aaa563c86976741a2261185f
Alex Tang <alex.m.tang@gmail.com>
2013-10-27 22:34:49 -0400

    Can eval code in editor now

--- console.js
+++ console.js
@@ -1,3 +1,29 @@
+var tabId = chrome.devtools.inspectedWindow.tabId;
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/twilight");
-editor.getSession().setMode("ace/mode/javascript");
+
+editor.setTheme("ace/theme/textmate");
+editor.session.setMode("ace/mode/javascript");
+editor.getSession().setUseWorker(false);
+editor.setShowPrintMargin(false);
+editor.setDisplayIndentGuides(true);
+editor.session.setUseSoftTabs(true);
+
+function execute() {
+  chrome.devtools.inspectedWindow.eval(
+    editor.session.getValue(),
+    function(result, isException) {
+    });
+}
+
+var executeOptions = {
+  name: "execute",
+  exec: execute,
+  bindKey: "Ctrl-Return|Command-Return"
+};
+
+editor.on("change", function(err) {
+  localStorage.setItem("state" + tabId, editor.session.getValue());
+});
+
+editor.commands.addCommand(executeOptions);
+editor.session.setValue(localStorage.getItem("state" + tabId));
