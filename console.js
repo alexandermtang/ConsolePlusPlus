a54cbeb4e7cc377ddcd80a9c2885269135ac877c
Alex Tang <alex.m.tang@gmail.com>
2013-10-28 23:29:42 -0400

    Add results ACE editor

--- console.js
+++ console.js
@@ -1,12 +1,19 @@
 var tabId = chrome.devtools.inspectedWindow.tabId;
 var editor = ace.edit("editor");
-
 editor.setTheme("ace/theme/textmate");
 editor.session.setMode("ace/mode/javascript");
 editor.getSession().setUseWorker(false);
 editor.setShowPrintMargin(false);
 editor.setDisplayIndentGuides(true);
 editor.session.setUseSoftTabs(true);
+
+var results = ace.edit("results");
+results.session.setMode("ace/mode/asciidoc");
+results.setReadOnly(true);
+results.getSession().setUseWorker(false);
+results.setShowPrintMargin(false);
+results.renderer.setShowGutter(false);
+results.setHighlightActiveLine(false);
 
 function execute() {
   chrome.devtools.inspectedWindow.eval(
